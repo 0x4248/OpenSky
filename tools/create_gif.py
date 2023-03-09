a90f3666e0492c1ce9586dcb67df2bdd6e68416f
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-03-09 19:08:50 +0000

    Create readme gif generator

---
+++ tools/create_gif.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+PATH = "dataset/"
+
+def create_gif():
+    os.system("ffmpeg -y -framerate 2 -i gif_tmp/%*.jpg  readme_animation.gif")
+
+if __name__ == "__main__":
+    picked = []
+    for i in range(3):
+        while True:
+            choice = random.choice(os.listdir(PATH))
+            if choice not in picked:
+                break
+        picked.append(choice)
+    os.mkdir("gif_tmp")
+    for i in range(len(picked)):
+        os.system("cp "+PATH+picked[i]+"/processed/1000/"+"* gif_tmp")
+        for i in range(len(os.listdir("gif_tmp"))):
+            os.rename("gif_tmp/"+os.listdir("gif_tmp")[i], "gif_tmp/"+str(random.randint(0, 100000000))+".jpg")
+    create_gif()
