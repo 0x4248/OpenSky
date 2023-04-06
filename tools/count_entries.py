081f9b6748292e799a012a3d88c446a7108ce686
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-06 12:03:52 +0100

    Add dataset image counter
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- tools/count_entries.py
+++ tools/count_entries.py
@@ -1,5 +1,22 @@
 import os
 
+import os
+
+def count_images():
+    dataset_dir = "dataset"
+    count = 0
+    for subdir in os.listdir(dataset_dir):
+        subdir_path = os.path.join(dataset_dir, subdir)
+        if os.path.isdir(subdir_path):
+            raw_dir = os.path.join(subdir_path, "raw")
+            if os.path.isdir(raw_dir):
+                for filename in os.listdir(raw_dir):
+                    if os.path.isfile(os.path.join(raw_dir, filename)):
+                        count += 1
+    return count
+                
+
+
 if __name__ == "__main__":
     count = 0
     for file in os.listdir("dataset"):
@@ -9,5 +26,6 @@ if __name__ == "__main__":
         readme = f.read()
     lines = readme.split("\n")
     lines[3] = "There are `"+str(count)+"` entries in this dataset."
+    lines[5] = "There are `"+str(count_images())+"` images in this dataset."
     with open("README.md", "w") as f:
         f.write("\n".join(lines))
